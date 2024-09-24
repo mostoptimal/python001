3382ac781d40c91816754212f9af030c845f2e4c
Mo <mostoptimal@hotmail.com>
2024-09-24 15:24:43 +0200

    add: Initial commit

---
+++ main.py
@@ -0,0 +1,17 @@
+"""
+main.py
+
+This module contains a simple Python script that prints "Hello World!" to the console
+and displays the current Python version.
+
+Functions:
+	None
+
+Usage:
+	Run this script directly to see the output.
+"""
+import sys
+
+print("Hello World!")
+
+print(sys.version)
